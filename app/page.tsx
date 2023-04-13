bfd0d9dbb762a4be6cda19493397f2569ea5616e
lju <veryveryeasily@gmail.com>
2023-04-13 05:09:35 -0700

    small cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,24 @@ import PromptControls from "./inputs";
 export default async function Home() {
   const messages = await db.message.findMany();
   const prompt = (await db.prompt.findFirst()) as Prompt;
-  assert(prompt, "No prompt found");
 
   return (
     <main className="flex min-h-screen">
-      <div className="p-8">
-        <div className="flex flex-col items-center justify-between">
-          <div>Hello world!</div>
-          <div>{prompt.content}</div>
-          <PromptControls />
-          <div className="flex flex-col">
-            {messages.map((message) => {
-              return (
-                <div key={message.id} className="flex">
-                  <div className="px-8">{message.role}</div>
-                  <div>{message.content}</div>
-                </div>
-              );
-            })}
-          </div>
+      <section className="flex flex-col items-center justify-between flex-1 p-8">
+        <div>Hello world!</div>
+        {prompt && <div>{prompt.content}</div>}
+        <PromptControls />
+        <div className="flex flex-col">
+          {messages.map((message) => {
+            return (
+              <div key={message.id} className="flex">
+                <div className="px-8">{message.role}</div>
+                <div>{message.content}</div>
+              </div>
+            );
+          })}
         </div>
-      </div>
+      </section>
     </main>
   );
 }
